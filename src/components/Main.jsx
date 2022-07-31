04ca7cde821b0817a057c41ef35969f11fc5d87c
Devmidorka <urvanovdmitriy@gmail.com>
2022-07-31 17:00:16 +0300

    Using truncate movie overview

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -29,7 +29,9 @@ const Main = () => {
                         <button className='border text-white border-gray-300 py-2 px-5 ml-4'>Watch</button>
                     </div>
                     <p className='text-gray-400 text-sm'>Released: {movie?.release_date}</p>
-                    <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] my-4'>{movie?.overview}</p>
+                    <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] my-4'>
+                        {truncateString(movie?.overview, 150)}
+                    </p>
                 </div>
             </div>
         </div>
