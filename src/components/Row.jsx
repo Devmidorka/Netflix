612b43c814d99843dd46c485fae349493a03a681
Devmidorka <urvanovdmitriy@gmail.com>
2022-08-01 19:08:24 +0300

    Slider functionality

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,10 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import {MdChevronLeft, MdChevronRight} from "react-icons/md";
 
 import Movie from "./Movie";
 
 const Row = ({title = '', fetchDataFunction = () => {}}) => {
 
     const [movies, setMovies] = useState([])
+    const sliderRef = useRef(null)
 
     useEffect(() => {
         fetchDataFunction()
@@ -14,17 +16,27 @@ const Row = ({title = '', fetchDataFunction = () => {}}) => {
 
     }, [])
 
+    const slideLeft = (slider) => {
+        slider.scrollLeft -= 500
+    }
+
+    const slideRight = (slider) => {
+        slider.scrollLeft += 500
+    }
+
     return (
         <>
             <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-            <div className='relative flex items-center'>
-                <div id="slider" className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
+            <div className='relative flex items-center group'>
+                <MdChevronLeft onClick={() => slideLeft(sliderRef.current)} className='bg-white rounded-full absolute  left-0 opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40}/>
+                <div ref={sliderRef} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
                     {movies.length > 0 ?
                         movies.map(movie => <Movie key={movie.id} movie={movie}/>)
                         :
                         null
                     }
                 </div>
+                <MdChevronRight onClick={() => slideRight(sliderRef.current)} className='bg-white rounded-full absolute right-0 opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40}/>
             </div>
         </>
     );
