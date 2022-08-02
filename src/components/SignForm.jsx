82d2939bf9bd5fa5df21a4bc9394bd69ac0734f0
Devmidorka <urvanovdmitriy@gmail.com>
2022-08-02 22:41:21 +0300

    SignIn, SignOut components

--- src/components/SignForm.jsx
+++ src/components/SignForm.jsx
@@ -1,27 +1,8 @@
-import React, {useState} from 'react';
-import {Link, useNavigate} from "react-router-dom";
-import {useUserAuth} from "../context/AuthContext";
+import React from 'react';
+import {Link} from "react-router-dom";
 import Button from "./UI/Button";
 
-const SignForm = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const {signUp} = useUserAuth()
-    const [error, setError] = useState('')
-    const navigate = useNavigate()
-    const handleSubmit = async(event) => {
-        event.preventDefault()
-        setError('')
-        try{
-            await signUp(email, password)
-            navigate('/')
-        }catch(error){
-            console.log(error)
-            setError(error.message)
-        }
-    }
-
-
+const SignForm = ({error, handleSubmit, setEmail, setPassword, type}) => {
     return (
         <form className='w-full flex flex-col py-4' onSubmit={handleSubmit}>
             {error ? <p className='p-3 bg-red-400 my-2'>{error}</p> : null }
@@ -39,16 +20,18 @@ const SignForm = () => {
                 placeholder='Password'
                 autoComplete='password'
             />
-            <Button className='my-4'>Sign Up</Button>
+            <Button className='my-4'>{type === 'login' ? 'Sign In' : 'Sign Up'}</Button>
             <div className='flex justify-between items-center text-sm text-gray-500 py-6'>
                 <label><input type="checkbox"/> Remember me</label>
                 <p>Need Help?</p>
             </div>
             <p className='py-8'>
-                <span className='text-gray-600 pr-2'>Already subscribed to Netflix?</span>
-                <Link to='/login'>
-                    Sign In
-                </Link>
+                <span className='text-gray-600 pr-2'>
+                    {type === 'login' ? 'New to Netflix?' : 'Already subscribed to Netflix?'}
+
+                </span>
+                {type === 'login' ?<Link to='sign_up'>Sign Up</Link> : <Link to='/login'> Sign In </Link>}
+
             </p>
         </form>
     );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,37 +1,38 @@
 import React, {useState} from 'react';
-import SignForm from "../components/SignForm";
-import Modal from "../components/UI/Modal";
 import {useUserAuth} from "../context/AuthContext";
+import Modal from "../components/UI/Modal";
+import SignForm from "../components/SignForm";
 import {useNavigate} from "react-router-dom";
 
+const Login = () => {
 
-const SignUp = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const {signUp} = useUserAuth()
+    const {logIn} = useUserAuth()
     const [error, setError] = useState('')
     const navigate = useNavigate()
     const handleSubmit = async(event) => {
         event.preventDefault()
         setError('')
         try{
-            await signUp(email, password)
+            await logIn(email, password)
             navigate('/')
         }catch(error){
             console.log(error)
             setError(error.message)
         }
     }
+
     return (
         <div className='w-full h-screen'>
             <img className='block absolute w-full h-full object-cover' src="https://assets.nflxext.com/ffe/siteui/vlv3/c2578c37-8569-4f88-b8f1-67a26a9ddcdd/33c67853-dd11-42fd-b8ef-87b9cf28e06f/DE-en-20220725-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt=""/>
             <div className='absolute bg-black/60 top-0 left-0 w-full h-screen'/>
             <Modal>
-                <h1 className='text-3xl font-bold'>Sign Up</h1>
-                <SignForm error={error} handleSubmit={handleSubmit} setEmail={setEmail} setPassword={setPassword}/>
+                <h1 className='text-3xl font-bold'>Sign In</h1>
+                <SignForm error={error} handleSubmit={handleSubmit} setEmail={setEmail} setPassword={setPassword} type={'login'}/>
             </Modal>
         </div>
     );
 };
 
-export default SignUp;
+export default Login;
