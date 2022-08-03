99b85e55baf3166a8d96e03ec489a20a3497c9a4
Devmidorka <urvanovdmitriy@gmail.com>
2022-08-03 21:50:34 +0300

    Movie plug

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,6 +4,7 @@ import {useUserAuth} from '../context/AuthContext'
 import {db} from "../firebase/firebase";
 import {arrayUnion, doc, updateDoc} from "firebase/firestore"
 import {AiOutlineClose} from 'react-icons/ai'
+import moviePlug from '../assets/movie.webp'
 
 const Movie = ({movie, isMyMovie= false, deleteMovies = () => {}}) => {
 
@@ -34,33 +35,36 @@ const Movie = ({movie, isMyMovie= false, deleteMovies = () => {}}) => {
         <div
             className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] cursor-pointer inline-block relative p-2'
         >
-            {movie?.backdrop_path &&
-                <>
-                    <img
-                        src={`https://image.tmdb.org/t/p/w300${movie?.backdrop_path}`}
-                        alt={movie.title}
-                    />
-                    <div
-                        className="absolute top-0 left-0 w-full h-full opacity-0 text-white  hover:bg-black/80 hover:opacity-100 "
-                    >
-                        <p className='text-xs md:text-sm font-bold flex justify-center items-center h-full text-center whitespace-normal px-3'>
-                            {movie?.title}
-                        </p>
-                        {isMyMovie ?
-                            <p onClick={() => deleteMovies(movie.id, movieRef)} className='absolute text-gray-300 top-4 right-4'><AiOutlineClose/></p>
-                            :
-                            <p onClick={saveMovie}>
-                                {like ?
-                                    <FaHeart className='absolute top-4 left-4 text-gray-300'/>
-                                    :
-                                    <FaRegHeart  className='absolute top-4 left-4 text-gray-300'/>
-                                }
-                            </p>
-                        }
-
-                    </div>
-                </>
+            {movie?.backdrop_path ?
+                <img
+                    src={`https://image.tmdb.org/t/p/w300${movie?.backdrop_path}`}
+                    alt={movie.title}
+                />
+                :
+                <img
+                    src={moviePlug}
+                    alt={movie.title}
+                />
             }
+            <div
+                className="absolute top-0 left-0 w-full h-full opacity-0 text-white  hover:bg-black/80 hover:opacity-100 "
+            >
+                <p className='text-xs md:text-sm font-bold flex justify-center items-center h-full text-center whitespace-normal px-3'>
+                    {movie?.title}
+                </p>
+                {isMyMovie ?
+                    <p onClick={() => deleteMovies(movie.id, movieRef)} className='absolute text-gray-300 top-4 right-4'><AiOutlineClose/></p>
+                    :
+                    <p onClick={saveMovie}>
+                        {like ?
+                            <FaHeart className='absolute top-4 left-4 text-gray-300'/>
+                            :
+                            <FaRegHeart  className='absolute top-4 left-4 text-gray-300'/>
+                        }
+                    </p>
+                }
+
+            </div>
         </div>
     );
 };
